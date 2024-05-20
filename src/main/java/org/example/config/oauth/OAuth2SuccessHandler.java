499e8d9b1e2467b380324437f182208dd291dd3a
limbaba1120 <kwlim1120ss@gmail.com>
2024-05-20 19:54:02 +0900

    fix: url changed

--- src/main/java/org/example/config/oauth/OAuth2SuccessHandler.java
+++ src/main/java/org/example/config/oauth/OAuth2SuccessHandler.java
@@ -25,7 +25,7 @@ public class OAuth2SuccessHandler extends SimpleUrlAuthenticationSuccessHandler 
     public static final String ACCESS_TOKEN_COOKIE_NAME = "token";
     public static final Duration REFRESH_TOKEN_DURATION = Duration.ofDays(14);
     public static final Duration ACCESS_TOKEN_DURATION = Duration.ofDays(1);
-    public static final String REDIRECT_PATH = "http://localhost:3000/api/auth/login"; //redirect 경로
+    public static final String REDIRECT_PATH = "http://prod-coffeechat-service.ap-northeast-2.elasticbeanstalk.com/api/auth/login"; //redirect 경로
 
 
     private final TokenProvider tokenProvider;
