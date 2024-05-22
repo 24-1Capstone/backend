21d242922a22d8ba81461b100117472cca6cc6a7
limbaba1120 <kwlim1120ss@gmail.com>
2024-05-22 13:58:33 +0900

    test: changed to jar redo

--- src/main/java/org/example/SpringBootDeveloperApplication.java
+++ src/main/java/org/example/SpringBootDeveloperApplication.java
@@ -2,8 +2,6 @@ package org.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
@@ -14,15 +12,10 @@ import java.util.UUID;
 
 @EnableJpaAuditing // created_at, updated_at 자동 업데이트
 @SpringBootApplication
-public class SpringBootDeveloperApplication extends SpringBootServletInitializer {
+public class SpringBootDeveloperApplication  {
     public static void main(String[] args) {
         SpringApplication.run(SpringBootDeveloperApplication.class, args);
     }
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(SpringBootDeveloperApplication.class);
-    }
-
     @Bean
     public BCryptPasswordEncoder bCryptPasswordEncoder() {
         return new BCryptPasswordEncoder();
