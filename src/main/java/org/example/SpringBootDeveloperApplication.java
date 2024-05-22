0a0933d27b6f1c791b0fe1b0e07508e9703afb8f
limbaba1120 <kwlim1120ss@gmail.com>
2024-05-22 13:40:59 +0900

    test: war file 배포 test

--- src/main/java/org/example/SpringBootDeveloperApplication.java
+++ src/main/java/org/example/SpringBootDeveloperApplication.java
@@ -2,6 +2,8 @@ package org.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
@@ -12,10 +14,15 @@ import java.util.UUID;
 
 @EnableJpaAuditing // created_at, updated_at 자동 업데이트
 @SpringBootApplication
-public class SpringBootDeveloperApplication {
+public class SpringBootDeveloperApplication extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(SpringBootDeveloperApplication.class, args);
     }
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(SpringBootDeveloperApplication.class);
+    }
+
     @Bean
     public BCryptPasswordEncoder bCryptPasswordEncoder() {
         return new BCryptPasswordEncoder();
