761caf552fc7f0d4c1e9bec018b9b899945125c6
limbaba1120 <kwlim1120ss@gmail.com>
2024-05-23 15:49:26 +0900

    feat: globalexceptionhandler 추가

---
+++ src/main/java/org/example/exception/GlobalExceptionHandler.java
@@ -0,0 +1,19 @@
+package org.example.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.reactive.result.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Object> handleAllExceptions(Exception ex, WebRequest request) {
+        // 예외 로그 기록
+        ex.printStackTrace();
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
