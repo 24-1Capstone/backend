01b3c3c06724404923c7c5790ba9582d6c9e23f9
limbaba1120 <kwlim1120ss@gmail.com>
2024-05-24 11:23:23 +0900

    feat: add exception

--- src/main/java/org/example/GlobalExceptionHandler.java
+++ src/main/java/org/example/GlobalExceptionHandler.java
@@ -1,21 +1,26 @@
 package org.example;
 
+import lombok.extern.slf4j.Slf4j;
 import org.example.exception.response.ExceptionResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.reactive.result.method.annotation.ResponseEntityExceptionHandler;
 
 import java.time.LocalDateTime;
 
-@ControllerAdvice
+@RestControllerAdvice(basePackages = "org.example")
+@Slf4j
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(Exception.class)
     public final ResponseEntity<ExceptionResponse> handleAllExceptions(Exception ex, WebRequest request) {
         ExceptionResponse exceptionResponse = new ExceptionResponse(LocalDateTime.now(), ex.getMessage(), request.getDescription(true));
+        log.error("Error message: {}", ex.getMessage());
+        ex.printStackTrace();
         return new ResponseEntity<>(exceptionResponse, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
