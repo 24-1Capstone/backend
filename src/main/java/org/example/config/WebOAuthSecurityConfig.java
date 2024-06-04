de61bef1cf30d943d648df6066fafb806fda1606
limbaba1120 <kwlim1120ss@gmail.com>
2024-06-04 11:15:39 +0900

    feat: login failure handler 추가

--- src/main/java/org/example/config/WebOAuthSecurityConfig.java
+++ src/main/java/org/example/config/WebOAuthSecurityConfig.java
@@ -1,10 +1,7 @@
 package org.example.config;
 import lombok.RequiredArgsConstructor;
 import org.example.config.jwt.TokenProvider;
-import org.example.config.oauth.OAuth2AuthorizationRequestBasedOnCookieRepository;
-import org.example.config.oauth.OAuth2LogoutSuccessHandler;
-import org.example.config.oauth.OAuth2SuccessHandler;
-import org.example.config.oauth.OAuth2UserCustomService;
+import org.example.config.oauth.*;
 import org.example.user.application.token.RefreshTokenService;
 import org.example.user.repository.token.RefreshTokenRepository;
 import org.example.user.application.member.UserService;
@@ -18,6 +15,7 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.Http403ForbiddenEntryPoint;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.web.cors.CorsConfiguration;
@@ -37,7 +35,6 @@ public class WebOAuthSecurityConfig {
     private final TokenProvider tokenProvider;
     private final RefreshTokenRepository refreshTokenRepository;
     private final UserService userService;
-    private final RefreshTokenService refreshTokenService;
 
 //    @Bean
 //    public WebSecurityCustomizer configure() {
@@ -88,6 +85,7 @@ public class WebOAuthSecurityConfig {
                 .authorizationRequestRepository(oAuth2AuthorizationRequestBasedOnCookieRepository())//Authorization 요청과 관련된 상태 저장
                 .and()
                 .successHandler(oAuth2SuccessHandler()) // 인증 성공 시 실행할 핸들러
+                .failureHandler(oAuth2FailureHandler())
                 .userInfoEndpoint()
                 .userService(oAuth2UserCustomService);
 
@@ -97,6 +95,7 @@ public class WebOAuthSecurityConfig {
                 .deleteCookies("refresh_token", "access_token")
                 .clearAuthentication(true)
                 .invalidateHttpSession(true);
+
 
 
         // /api로 시작하는 url인 경우 401 상태 코드를 반환하도록 예외 처리
@@ -132,7 +131,6 @@ public class WebOAuthSecurityConfig {
     }
 
 
-
     @Bean
     public OAuth2LogoutSuccessHandler logoutSuccessHandler() {
         return new OAuth2LogoutSuccessHandler(refreshTokenRepository, tokenProvider);
@@ -146,6 +144,11 @@ public class WebOAuthSecurityConfig {
                 userService
         );
     }
+    @Bean
+    public CustomOAuth2FailureHandler oAuth2FailureHandler() {
+        return new CustomOAuth2FailureHandler();
+    }
+
 
     @Bean
     public TokenAuthenticationFilter tokenAuthenticationFilter() {
